87ec929f72429110091a4ccd929d45961e2a2ccc
Bruno Silvano Costa <brunosilvanocosta@outlook.com>
2021-12-28 00:43:48 +0100

    Remove seconds from clock

--- app/index.ts
+++ app/index.ts
@@ -6,17 +6,16 @@ import { formatHours, zeroPad } from '../common/utils';
 const myLabel = document.getElementById('myLabel');
 
 // Clock settings
-clock.granularity = "seconds";
+clock.granularity = "minutes";
 
 clock.ontick = ev => {
   // Get clock values
   let today = ev.date;
   let hours = today.getHours();
   let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
 
   // Format display time
-  let formattedTime = `${formatHours(hours, preferences.clockDisplay)}:${zeroPad(minutes)}:${zeroPad(seconds)}`;
+  let formattedTime = `${formatHours(hours, preferences.clockDisplay)}:${zeroPad(minutes)}`;
 
   // Update value of text
   myLabel.text = formattedTime;
