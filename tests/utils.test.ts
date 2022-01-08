7de7d03949ba5d77291c8e09b3792eeb3b101b02
Bruno Silvano Costa <brunosilvanocosta@outlook.com>
2022-01-08 13:58:20 +0100

    Fix formatHours tests

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -9,31 +9,29 @@ describe("formatHours", () => {
     });
 
     test("should format 12 to 12", () => {
-      expect(formatHours(3, "12h")).toBe("3");
+      expect(formatHours(12, "12h")).toBe("12");
     });
 
     test("should format 13 to 1", () => {
-      expect(formatHours(3, "12h")).toBe("3");
-    });
-
-    test("should format 24 to 0", () => {
-      expect(formatHours(3, "12h")).toBe("3");
+      expect(formatHours(13, "12h")).toBe("1");
     });
   });
 
   // 24h
   describe("to 24h", () => {
     test("should format 3 to 3", () => {
-      expect(formatHours(3, "12h")).toBe("3");
+      expect(formatHours(3, "24h")).toBe("03");
     });
 
     test("should format 12 to 12", () => {
-      expect(formatHours(3, "12h")).toBe("3");
+      expect(formatHours(12, "24h")).toBe("12");
     });
 
     test("should format 13 to 13", () => {
-      expect(formatHours(3, "12h")).toBe("3");
+      expect(formatHours(13, "24h")).toBe("13");
     });
+  });
+});
 
     test("should format 24 to 0", () => {
       expect(formatHours(3, "12h")).toBe("3");
